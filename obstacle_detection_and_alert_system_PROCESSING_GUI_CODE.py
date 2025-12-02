60db579afcf625a7c9c0c4a9eaac0a0762cec92c
Saish Kadam <163175475+SaishKadam3112@users.noreply.github.com>
2025-12-02 15:12:32 +0530

    initial if loop

--- obstacle_detection_and_alert_system_PROCESSING_GUI_CODE.py
+++ obstacle_detection_and_alert_system_PROCESSING_GUI_CODE.py
@@ -12,13 +12,28 @@ void setup()
   myPort.bufferUntil('\n');
 }
 
-void draw() {
+void draw() 
+{
   background(0);
   drawRadar();
   drawSweepLine(angle);
   drawObject(angle, distance);
 }
 
-void serialEvent(Serial myPort) {
+void serialEvent(Serial myPort) 
+{
   String data = myPort.readStringUntil('\n');
+
+ if (data != null)
+ {
+    data = trim(data);
+    String values[] = split(data, ',');
+    if (values.length == 2)
+    {
+      angle = float(values[0]);
+      distance = float(values[1]);
+    }
+  }
+}
+
 
